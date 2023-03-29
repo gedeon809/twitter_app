8d367c7d27cf371bad9074643017b347dd7ec890
Gedeon Kakona <gedeonkakona@gmail.com>
2023-03-29 09:09:24 +0200

    app layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,11 @@
+import Header from '@/components/Header'
+
+
 export default function Home() {
-  return <div className="text-3xl text-sky-500">hello word</div>;
+  return (
+    <>
+    <Header label="Home"/>
+    </>
+  )
+  
 }
